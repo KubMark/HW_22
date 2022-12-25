6f14e2b038b16c47b003001f29a3bcd057b5be1d
macbook <mypur86@yahoo.com>
2022-12-26 02:07:23 +0300

    first_commit

--- part1/practice/abstract_classes/main.py
+++ part1/practice/abstract_classes/main.py
@@ -39,11 +39,77 @@
 
 # код должен выполняться не выбрасывая исключений
 
-# TODO напишите Ваш код здесь
+from abc import ABC, abstractmethod
 
+class Transport(ABC):
+    @abstractmethod
+    def start_engine(self):
+        pass
+
+    @abstractmethod
+    def stop_engine(self):
+        pass
+
+    @abstractmethod
+    def move(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+class Boat(Transport):
+    def start_engine(self):
+        print("Катер тарахтя завелся")
+
+    def stop_engine(self):
+        print("Двигатель катера заглушился")
+
+    def move(self):
+        print("Катер набирает скорость")
+
+    def stop(self):
+        print("Катер остановился")
+
+
+class Car(Transport):
+    def start_engine(self):
+        print("Известная в своих кругах машина по прозвищу 'кабан' завелась урча 12 горшковым двигателем:)")
+
+    def stop_engine(self):
+        print("Двигатель любимца заглушен и теперь можно услышать пение птиц)))")
+
+    def move(self):
+        print("Кабан с рёвом стремительно набирает скорость")
+
+    def stop(self):
+        print("Благодаря хорошей тормозной системе, махина довольно быстро останавливается")
+
+
+class Electroscooter(Transport):
+    def start_engine(self):
+        print("Словно Тесла, двухколесный дьявол включился и мигнул светодиодом:)")
+
+    def stop_engine(self):
+        print("Дважды мигнув светодиодом мотор агрегата отключился")
+
+    def move(self):
+        print("Быстрый электроскутер уже набирает скорость")
+
+    def stop(self):
+        print("Ещё одна успешная поездка, без проишествий. Скутер остановился")
+
+
+class Person:
+    def use_transport(self, transport: Transport):
+        transport.start_engine()
+        transport.move()
+        transport.stop()
+        transport.stop_engine()
 
 # Отрезок кода для самопроверки.
 # Запустите его, после того как выполните задание
+
 if __name__ == '__main__':
     boat = Boat()
     car = Car()
--- part1/practice/bad_smell_long_parameter_list/main.py
+++ part1/practice/bad_smell_long_parameter_list/main.py
@@ -8,43 +8,32 @@
 # - скорость
 # В этом примере есть сразу несколько запахов плохого кода. Исправьте их
 #   (длинный метод, длинный список параметров)
-
-
 class Unit:
-    def move(self, field, x_coord, y_coord, direction, is_fly, crawl, speed = 1):
+    def __init__(self, field, state, speed=1):
+        self.field = field
+        self.speed = speed
+        self.state = state
 
-        if is_fly and crawl:
-            raise ValueError('Рожденный ползать летать не должен!')
+    def _get_speed(self):
+        if self.state == 'fly':
+            self.speed = self.speed * 1.2
+        elif self.state == 'crawl':
+            self.speed = self.speed * 0.5
+        else:
+            raise ValueError("Unknown move mode")
 
-        if is_fly:
-            speed *= 1.2
-            if direction == 'UP':
-                new_y = y_coord + speed
-                new_x = x_coord
-            elif direction == 'DOWN':
-                new_y = y_coord - speed
-                new_x = x_coord
-            elif direction == 'LEFT':
-                new_y = y_coord
-                new_x = x_coord - speed
-            elif direction == 'RIGTH':
-                new_y = y_coord
-                new_x = x_coord + speed
-        if crawl:
-            speed *= 0.5
-            if direction == 'UP':
-                new_y = y_coord + speed
-                new_x = x_coord
-            elif direction == 'DOWN':
-                new_y = y_coord - speed
-                new_x = x_coord
-            elif direction == 'LEFT':
-                new_y = y_coord
-                new_x = x_coord - speed
-            elif direction == 'RIGTH':
-                new_y = y_coord
-                new_x = x_coord + speed
+    def move(self, x_coord, y_coord, direction):
+        if direction == 'UP':
+            new_y = y_coord + self.speed
+            new_x = x_coord
+        elif direction == 'DOWN':
+            new_y = y_coord - self.speed
+            new_x = x_coord
+        elif direction == 'LEFT':
+            new_y = y_coord
+            new_x = x_coord - self.speed
+        elif direction == 'RIGTH':
+            new_y = y_coord
+            new_x = x_coord + self.speed
 
-            field.set_unit(x=new_x, y=new_y, unit=self)
-
-#     ...
+        self.field.set_unit(x=new_x, y=new_y, unit=self)
